33d357af4a72bac729c59f2f65b94e589afc572b
Eli Richmond <erichmond_33@Elis-MacBook-Pro.local>
2022-04-10 18:50:43 -0500

    heroku fixes?

--- modified_dibels/settings.py
+++ modified_dibels/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,5 +136,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #AUTH_USER_MODEL = 'dibels_test.User'
 
 
-django_heroku.settings(locals())
 
