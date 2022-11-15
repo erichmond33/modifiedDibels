81a7ca26e60ffeb0660d36f8b283e7d9a61b76be
Eli Richmond <erichmond_33@Elis-MacBook-Pro.local>
2022-11-15 10:27:38 -0600

    fix admin.py

--- dibels_test/admin.py
+++ dibels_test/admin.py
@@ -3,14 +3,14 @@ from django.contrib import admin
 
 from .models import *
 # Register your models here.
-admin.site.register(Sentence, SentenceAdmin)
-admin.site.register(mazeTest, mazeTestAdmin)
-admin.site.register(mazeQuestionAttempt, mazeQuestionAttemptAdmin)
-admin.site.register(imageTest, imageTestAdmin)
-admin.site.register(imageQuestionAttempt, imageQuestionAttemptAdmin)
-admin.site.register(queuedMazeQuestion, queuedMazeQuestionAdmin)
-admin.site.register(queuedImageQuestion, queuedImageQuestionAdmin)
-admin.site.register(Font, FontAdmin)
-admin.site.register(Image, ImageAdmin)
+admin.site.register(Sentence)
+admin.site.register(mazeTest)
+admin.site.register(mazeQuestionAttempt)
+admin.site.register(imageTest)
+admin.site.register(imageQuestionAttempt)
+admin.site.register(queuedMazeQuestion)
+admin.site.register(queuedImageQuestion)
+admin.site.register(Font)
+admin.site.register(Image)
 
 
